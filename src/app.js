38a239af4caf686c2f9b42437b6875b1b18a8423
Jesús Merino <jmmerino@gmail.com>
2015-08-08 12:14:11 +0200

    First ajax call

--- src/app.js
+++ src/app.js
@@ -7,10 +7,10 @@ var splashWindow = new UI.Window();
 
 // Text element to inform user
 var text = new UI.Text({
-  position: new Vector2(0, 20),
+  position: new Vector2(0, 0),
   size: new Vector2(144, 168),
   text:'Busal',
-  font:'GOTHIC_28_BOLD',
+  font:'bitham-42-light',
   color:'black',
   textOverflow:'wrap',
   textAlign:'center',
@@ -18,10 +18,10 @@ var text = new UI.Text({
 });
 
 var subtitle = new UI.Text({
-  position: new Vector2(0, 60),
+  position: new Vector2(0, 100),
   size: new Vector2(144, 168),
   text:'Cargando líneas...',
-  font:'GOTHIC_20_BOLD',
+  font:'gothic-14',
   color:'black',
   textOverflow:'wrap',
   textAlign:'center',
@@ -31,4 +31,18 @@ var subtitle = new UI.Text({
 // Add to splashWindow and show
 splashWindow.add(text);
 splashWindow.add(subtitle);
-splashWindow.show();
+splashWindow.show();  
+
+// Make request to busal.es
+ajax(
+  {
+    url:'http://busal.es/api/v0.1/lines',
+    type:'json'
+  },
+  function(data) {
+    console.log(data);
+  },
+  function(error) {
+    console.log('Download failed: ' + error);
+  }
+);
